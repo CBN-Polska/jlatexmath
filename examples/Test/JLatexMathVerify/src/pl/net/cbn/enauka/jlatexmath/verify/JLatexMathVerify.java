546a080576f4c08ec88f7be6db137e809c59f2d2
Artur Jackowski <artur.jackowski@gmail.com>
2018-09-05 13:42:36 +0200

    tests for EN-808, ENT-72

--- examples/Test/JLatexMathVerify/src/pl/net/cbn/enauka/jlatexmath/verify/JLatexMathVerify.java
+++ examples/Test/JLatexMathVerify/src/pl/net/cbn/enauka/jlatexmath/verify/JLatexMathVerify.java
@@ -15,13 +15,7 @@ import org.scilab.forge.jlatexmath.TeXIcon;
  * @author Artur Jackowski
  */
 public class JLatexMathVerify {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        String latex = "$\\neq 0";
-
+    private void openTexFindow(String latex) {
         TeXFormula formula = new TeXFormula(latex);
         TeXIcon icon = formula.new TeXIconBuilder().setStyle(TeXConstants.STYLE_DISPLAY)
                 .setSize(16)
@@ -29,7 +23,7 @@ public class JLatexMathVerify {
                 .setIsMaxWidth(true).setInterLineSpacing(TeXConstants.UNIT_PIXEL, 20f)
                 .build();
 
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame(latex.substring(0, Math.min(100, latex.length())));
         final JLabel label = new JLabel(icon);
         label.setMaximumSize(new Dimension(100, 300));
         label.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
@@ -38,6 +32,33 @@ public class JLatexMathVerify {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(640, 480);
         frame.setVisible(true);
+    }
+    
+    /**
+     * Should give "!=0!=0" but was incorrectly given "!=0!=00".
+     */
+    private void testNeqNeq() {
+        openTexFindow("$\\neq 0$$\\neq 0$");
+    }
+    
+    /**
+     * Was given java.lang.StackOverflowError if that exactly tex was called
+     * without any previous latex calls. This tex must be called first on clean
+     * JLatexmath memory (no other previous texformula creations).
+     */
+    private void testNeq() {
+        openTexFindow("$\\neq 0\\underbrace{a}_{\\neq 0} $");
+    }
+
+    private void test() {
+        testNeq();
+        testNeqNeq();
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new JLatexMathVerify().test();
     }
 
 }
